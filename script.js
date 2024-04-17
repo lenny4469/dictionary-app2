cd701c7287c96681a2d0922eddc71a24067abcd4
Lenny Muriithi Mawira <lenny.mawira@student.moringaschool.com>
2024-04-17 22:27:05 +0300

    search btn js

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchInput = document.getElementById('search-input');
+const speakBtn = document.getElementById('speak-btn');
+const searchBtn = document.getElementById('search-btn');
+const resultDiv = document.getElementById('result');
+searchInput.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        searchBtn.click();
+}})
